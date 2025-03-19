83b9c904ede3a0553ced83c7b6fc6d18cdda5919
Iurii Romanov <yromanov0@gmail.com>
2025-03-19 16:11:29 +0100

    simple controller

---
+++ src/main/java/com/example/spring_boot_docker/Controller.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings hello!";
+    }
+}
--- src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
+++ src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
@@ -2,20 +2,12 @@ package com.example.spring_boot_docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class SpringBootDockerApplication {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDockerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
 
 }
